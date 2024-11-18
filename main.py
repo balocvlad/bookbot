467d53da51ffbea3c9ca30101fab5867f493ee80
balocvlad <vlad.baloc@yahoo.com>
2024-11-18 18:57:52 +0200

    refactor

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        words = file_contents.split()
-        print(len(words))
+        print(count_words_book(file_contents))
+
+def count_words_book(book):
+    words = book.split()
+    return(len(words))
+
+
 main()
